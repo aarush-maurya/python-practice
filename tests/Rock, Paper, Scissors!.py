b7da2bba7b45a87715109f3bdb7d3cf0521bcd82
Aarush Maurya <148880435+aarush-maurya@users.noreply.github.com>
2025-06-28 13:48:34 +0530

    guess the number
    improved RPS and added guessing game

--- tests/Rock, Paper, Scissors!.py
+++ tests/Rock, Paper, Scissors!.py
@@ -1,4 +1,6 @@
 from getpass import getpass as input
+pl1_w =0
+pl2_w=0
 while True:
     print(f"\033[31m===Rock, Paper, Scissors!===\033[0m")
     print(f"Choose R,P or S")
@@ -6,28 +8,34 @@ while True:
     pl_2 = input(f"Player 2 move : ")
     if pl_1 == pl_2:
         print(f"Draw!")
-        exit()
+        continue
     elif pl_1 == 'R':
         if pl_2=='P':
             win= '2'
+            pl2_w += 1
         elif pl_2 == 'S':
             win ='1'
+            pl1_w += 1
         else:
             print(f"You have to type 'R' ,'P' or 'S'")
             exit()
     elif pl_1 == 'P':
         if pl_2 == 'R':
             win = '1'
+            pl1_w += 1
         elif pl_2 == 'S':
             win ='2'
+            pl2_w += 1
         else:
             print(f"You have to type 'R' ,'P' or 'S'")
             exit()
     elif pl_1 == 'S':
         if pl_2 == 'R':
             win = '2'
+            pl2_w += 1
         elif pl_2 == 'P':
             win ='1'
+            pl1_w += 1
         else:
             print(f"You have to type 'R' ,'P' or 'S'")
             exit()
@@ -44,6 +52,7 @@ while True:
         print(f"You either have to say 'yes' or 'no'")
         try_ = input(f"Do you wanna try again?(yes/no) : ")
         break
+print(f"Player 1 has won {pl1_w} times and Player 2 has won {pl2_w} times")
     
         
 
